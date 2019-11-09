07661edfae2bf8777a78011488e550526203371d
bcudina <bozidar.cudina@gmail.com>
2019-11-09 15:24:04 +0100

    druga III

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import Druga from "./druga";
 
 function App() {
   return (
     <div className="App">
       <h1>početna</h1>
+      <Druga />
     </div>
   );
 }
