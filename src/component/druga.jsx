4980ea87ef21d8a577e83c7babba78b345efe9c0
bcudina <bozidar.cudina@gmail.com>
2019-11-09 15:13:36 +0100

    novi  pocetak

---
+++ src/component/druga.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Druga() {
+  return (
+    <div className="App">
+      <h4>Druga strana</h4>
+    </div>
+  );
+}
+
+export default Druga;
---
+++ src/component/treca.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Treca() {
+  return (
+    <div className="App">
+      <h4>Treca strana</h4>
+    </div>
+  );
+}
+
+export default Treca;
