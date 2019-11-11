21a06d6e21c86d1123a31878183aa0e22218d699
bcudina <bozidar.cudina@gmail.com>
2019-11-11 13:56:30 +0100

    Revert "sve stabilno"
    This reverts commit 38f3d6e4ffc013274dd2becf4a025449d5117066.

--- src/App.js
+++ src/App.js
@@ -4,29 +4,17 @@ import "./component/myScss.scss";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Home from "./component/home";
 import Druga from "./component/druga";
 import Treca from "./component/treca";
-
 import Nav from "./component/navbar";
-
-const Pocetna = () => (
-  <div>
-    <Home />
-  </div>
-);
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Nav />
-        <Switch>
-          <Route path="/" exact component={Pocetna} />
-
-          <Route path="/druga" component={Druga} />
-          <Route path="/treca" component={Treca} />
-        </Switch>
+        <Route path="/druga" component={Druga} />
+        <Route path="/treca" component={Treca} />
       </div>
     </Router>
   );
